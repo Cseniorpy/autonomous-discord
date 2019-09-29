7eb957e0ed3c481163592b6ea9b32ad8d7cacb93
Cseniorpy <seniordeveloper345@gmail.com>
2019-09-30 00:08:30 +0300

    events has been upgraded

--- maindiscord.py
+++ maindiscord.py
@@ -11,7 +11,7 @@ import numpy
 
 
 bot = commands.Bot(command_prefix='.')
-TOKEN = 'NjI3ODEwODQ1OTcxMzE2NzM3.XZC7Ow.Qf2FZPuSqAQ-pD1IKAIbWMORXeE'
+TOKEN = 'NjI3ODEwODQ1OTcxMzE2NzM3.XZDoew.5IHRMZo3ROw2mmFeclni-pFROQ0'
 bot.remove_command('help')
 
 
@@ -49,13 +49,18 @@ async def on_ready():
 
     print("BOT is ready!")
 
-
+"""
 @bot.event 
 async def on_member_join(member):
 
+    channel = bot.get_channel(627921483934466072)
+    await channel.send(welcome_message)
+"""
+
+"""
     channel = bot.get_channel(627812771450454046)
     await channel.send("**Hoşgeldin {} :smile:**".format(member.mention))
-
+"""
 
 @bot.event 
 async def on_member_remove(member):
@@ -94,35 +99,46 @@ async def yardım(ctx):
 
 @bot.command()
 async def kanal(ctx):
+
+    if not ctx.channel in public_channel_list:
+        await ctx.channel.send("**Bu kanalı kullanmalısın :point_right: {0.mention}**".format(channel_bot_test))
+
+    else:
+
     
-    embed = discord.Embed(title='**KANAL REHBERİ**', description='**Sunucuda bulunan kanalların bulunduğu kategoriye göre bilgi sahibi olmak için rehberi okuyunuz**', color=0xfc0303)
+        embed = discord.Embed(title='**KANAL REHBERİ**', description='**Sunucuda bulunan kanalların bulunduğu kategoriye göre bilgi sahibi olmak için rehberi okuyunuz**', color=0xfc0303)
 
-    embed.add_field(name="**AUTONOMOUS**", value="```Proje hakkında genel bilgi almak için inceleyebilirsiniz```")
-    embed.add_field(name="**CHAT**", value="```Burada sohbet edebilirsiniz```")
-    embed.add_field(name="**PROJECT PROCESS**", value="```Bu bölümde, Projenin dört temel süreci hakkında bilgi edinebilirsiniz```")
-    embed.add_field(name="**ACADEMIC PROCESS**", value="```Road MAP süresince karşılaşacağınız sorunları buradan paylaşarak projenin gelişmesine katkı sağlayabilirsiniz```")
-    embed.add_field(name="**SUGGESTIONS&QUESTIONS**", value="```Proje, Discord ve BOT hakkındaki sorularınız ve görüşleriniz için burayı kullanabilirsiniz```")
-    embed.add_field(name="**VOICE CHANNELS**", value="```Bu bölümde sesli olarak sohbet edebilirsiniz```")
-    #embed.add_field(name="****", value="****")
+        embed.add_field(name="**AUTONOMOUS**", value="```Proje hakkında genel bilgi almak için inceleyebilirsiniz```")
+        embed.add_field(name="**CHAT**", value="```Burada sohbet edebilirsiniz```")
+        embed.add_field(name="**PROJECT PROCESS**", value="```Bu bölümde, Projenin dört temel süreci hakkında bilgi edinebilirsiniz```")
+        embed.add_field(name="**ACADEMIC PROCESS**", value="```Road MAP süresince karşılaşacağınız sorunları buradan paylaşarak projenin gelişmesine katkı sağlayabilirsiniz```")
+        embed.add_field(name="**SUGGESTIONS&QUESTIONS**", value="```Proje, Discord ve BOT hakkındaki sorularınız ve görüşleriniz için burayı kullanabilirsiniz```")
+        embed.add_field(name="**VOICE CHANNELS**", value="```Bu bölümde sesli olarak sohbet edebilirsiniz```")
+        #embed.add_field(name="****", value="****")
 
 
-    await ctx.send(embed=embed)
+        await ctx.send(embed=embed)
 
 @bot.command()
 async def sunucu(ctx):
 
-    #ID BELONGS TO DEVELOPER
-    server_admin = bot.get_user(627792025764495360)
-    server = ctx.author.guild
-    server_member_count = server.member_count
+    if not ctx.channel in public_channel_list:
+        await ctx.channel.send("**Bu kanalı kullanmalısın :point_right: {0.mention}**".format(channel_bot_test))
 
-    embed = discord.Embed(title=f'**{str(server).upper()}**', description='', color = 0x32a8a4)
-  
-    embed.add_field(name='**Yönetici**', value=f'```{server_admin.name}```')
-    embed.add_field(name='**Üye Sayısı**', value=f'```{int(server_member_count)-2}```')
-    embed.add_field(name='**Kuruluş zamanı**', value='```29/09/19```')
+    else:
 
-    await ctx.send(embed=embed)
+        #ID BELONGS TO DEVELOPER
+        server_admin = bot.get_user(627792025764495360)
+        server = ctx.author.guild
+        server_member_count = server.member_count
+
+        embed = discord.Embed(title=f'**{str(server).upper()}**', description='', color = 0x32a8a4)
+    
+        embed.add_field(name='**Yönetici**', value=f'```{server_admin.name}```')
+        embed.add_field(name='**Üye Sayısı**', value=f'```{int(server_member_count)-2}```')
+        embed.add_field(name='**Kuruluş zamanı**', value='```29/09/19```')
+
+        await ctx.send(embed=embed)
 
 @bot.command()
 async def sil(ctx, amount: int):
